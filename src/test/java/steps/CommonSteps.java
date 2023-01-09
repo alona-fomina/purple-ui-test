a72cc8e47d4d2a8f46e9cc11d1d3192b75a84132
alona-fomina <alona.f@purple.com>
2023-01-09 12:53:30 -0600

    hot fix for each env

--- src/test/java/steps/CommonSteps.java
+++ src/test/java/steps/CommonSteps.java
@@ -7,17 +7,19 @@ import utils.seleniumUtils.Waiter;
 
 public class CommonSteps {
     @And("user waits {int} seconds")
-    public void userWaitsSeconds(int seconds) throws Exception{
+    public void userWaitsSeconds(int seconds) throws Exception {
         Waiter.sleep(seconds * 1000);
     }
 
     @Given("user goes to homepage")
     public void userGoesToHomepage() {
-        if (ConfigReader.getProperty("homepage").equals("https://features:watchoutforsharks@features.purple.com/")){
+        if (ConfigReader.getProperty("homepage").equals("https://features:watchoutforsharks@features.purple.com/")) {
             do {
                 Hooks.driver.get(ConfigReader.getProperty("homepage"));
             }
             while (Hooks.driver.getCurrentUrl().equals("https://purple.com/2"));
+        } else {
+            Hooks.driver.get(ConfigReader.getProperty("homepage"));
         }
 
     }
